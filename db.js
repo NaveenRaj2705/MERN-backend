83436fb08926672caf0ae9ec8d3d8fb22c7c723e
NaveenRaj2705 <naveenraj.r2023cce@sece.ac.in>
2025-01-30 22:22:10 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Use the 127.0.0.1 format for the connection string
-mongoose.connect('mongodb://127.0.0.1:27017/hospital');
+mongoose.connect('mongodb+srv://naveenraj272005:2005@cluster0.jcgie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 const db = mongoose.connection;
 
